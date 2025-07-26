004e7520ac6d25b9763e53bca6d358d5e03cd12c
gauravmundhada <148524422+gauravmundhada@users.noreply.github.com>
2025-07-26 20:38:27 +0530

    Time: 0 ms (100%), Space: 97.3 MB (86.85%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,21 +1,18 @@
-// we will find the greatest value to the right for each element and then profit update krege whichever max we get
+// Greedy approach
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<int> largestInRHS(n);
-        largestInRHS[n-1] = prices[n-1]; // last element k right mein kuch nahi h
 
-        // populate the vector
-        for (int i = n-2; i >= 0; i--) {
-            largestInRHS[i] = max(prices[i], largestInRHS[i+1]);
-        }
-
-        // find the profits and update the max
-        int maxP = INT_MIN;
-        for (int i = 0; i < n; i++) {
-            int profit = largestInRHS[i] - prices[i];
-            maxP = max(maxP, profit);
+        int buy_price = prices[0]; 
+        int maxP = 0;
+        
+        for (int i = 1; i < n; i++) {
+            if (prices[i] <= buy_price) { // aur saste mein mil rha maal
+                buy_price = prices[i];
+            } else {
+                maxP = max(maxP, prices[i] - buy_price);
+            }
         }
         return maxP;
     }
