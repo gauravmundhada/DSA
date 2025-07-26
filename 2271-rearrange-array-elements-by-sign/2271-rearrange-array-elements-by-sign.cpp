6731191d8607814ae0dd825fad72b4c5da535aca
gauravmundhada <148524422+gauravmundhada@users.noreply.github.com>
2025-07-26 20:56:21 +0530

    Time: 11 ms (32.66%), Space: 139 MB (26.96%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.cpp
@@ -6,7 +6,7 @@ public:
 
         vector<int> pos; // to store the positive elements
         vector<int> neg; // to store the negative elements
-        vector<int> ans; // final output
+        vector<int> ans(n); // final output
 
         for (int i = 0; i < n; i++) {
             if (nums[i] < 0) neg.push_back(nums[i]);
@@ -14,15 +14,9 @@ public:
         }
 
         // construct the final ans
-        int p = 0, m = 0;
-        for (int i = 0; i < n; i++) {
-            if (i % 2 == 0) { // means positive no is to be placed
-                ans.push_back(pos[p]);
-                p++;
-            } else {
-                ans.push_back(neg[m]);
-                m++;
-            }
+        for (int i = 0; i < n/2; i++) {
+            ans[2*i]   = pos[i];
+            ans[2*i+1] = neg[i];
         }
         return ans;
     }
