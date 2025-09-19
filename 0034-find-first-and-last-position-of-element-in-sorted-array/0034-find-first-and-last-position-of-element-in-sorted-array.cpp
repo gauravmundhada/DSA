aecdb0a3ac40f0693df7a1f5515d65bf4bad03a6
gauravmundhada <148524422+gauravmundhada@users.noreply.github.com>
2025-09-19 20:18:15 +0530

    Time: 0 ms (100%), Space: 17.5 MB (86.36%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,53 +1,31 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int firstOcc = getFirstOccurence(nums, target, 0, nums.size()-1);
-        int lastOcc = getLastOccurence(nums, target, 0, nums.size()-1);
-
-        return {firstOcc, lastOcc};
-    }
-
-    int getFirstOccurence(vector<int>& nums, int target, int l, int r) {
+        int l = 0, r = nums.size()-1;
 
         while (l <= r) {
             int m = l + (r-l)/2;
 
             if (nums[m] == target) {
-                if (m > 0 && nums[m-1] == target) {
-                    return getFirstOccurence(nums, target, l, m-1);
-                } else {
-                    return m;
+                // first check for first occ
+                int first = m;
+                int last  = m;
+                while (first > 0 && nums[m] == nums[first-1]) {
+                    first--;
                 }
+
+                while (last < nums.size()-1 && nums[m] == nums[last+1]) {
+                    last++;
+                }
+                return {first, last};
             }
 
             if (nums[m] > target) {
                 r = m-1;
             } else {
-                l = m + 1;
+                l = m+1;
             }
         }
-        return -1;
-    }
-
-    int getLastOccurence(vector<int>& nums, int target, int l, int r) {
-
-        while (l <= r) {
-            int m = l + (r-l)/2;
-
-            if (nums[m] == target) {
-                if (m < nums.size()-1 && nums[m+1] == target) {
-                    return getLastOccurence(nums, target, m+1, r);
-                } else {
-                    return m;
-                }
-            }
-
-            if (nums[m] > target) {
-                r = m-1;
-            } else {
-                l = m + 1;
-            }
-        }
-        return -1;
+        return {-1, -1};
     }
 };
