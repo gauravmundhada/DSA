1d12d861e4b9c84acf10ee2a309f1806c6d6f7da
gauravmundhada <148524422+gauravmundhada@users.noreply.github.com>
2026-02-06 15:02:41 +0530

    Time: 3 ms (24.92%), Space: 14.9 MB (31.84%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,74 +1,85 @@
-class Solution {
+class DSU {
 public:
-    vector<vector<int>> directions = {
-        {0,1}, {1, 0}, {-1, 0}, {0, -1}
-    };
+    vector<int> parent, rank;
 
-    void dfs(vector<vector<char>>& board, int m, int n, int i, int j, vector<int>& visited) {
-        visited[i*n+j] = 1;
-
-        for (auto& d : directions) {
-                int nx = i + d[0];
-                int ny = j + d[1];
-
-                // out of bound + not accessible + already processed cell
-                if (nx < 0 || ny < 0 || nx >= m || ny >= n || board[nx][ny] == 'X' || visited[nx*n+ny] == 1) continue;
-
-                dfs(board, m, n, nx, ny, visited);
-            }
+    DSU(int n) {
+        parent.resize(n);
+        rank.resize(n, 0);
+        for (int i = 0; i < n; i++)
+            parent[i] = i;
     }
 
-    void solve(vector<vector<char>>& board) {
-        int m = board.size();
-        int n = board[0].size();
+    int find(int x) {
+        if (parent[x] != x)
+            parent[x] = find(parent[x]);
+        return parent[x];
+    }
 
-        // keep track of cell which is not to be marked
-        vector<int> visited(m*n, 0);
+    void unite(int x, int y) {
+        int px = find(x);
+        int py = find(y);
+        if (px == py) return;
 
-        //queue<pair<int, int>> q;
-
-        for (int i = 0; i < n; i++) {
-            if (board[0][i] == 'O') {
-                //q.push({0, i});
-                dfs(board, m, n, 0, i, visited);
-            }
-
-            if (board[m-1][i] == 'O') {
-                //q.push({m-1, i});
-                dfs(board, m, n, m-1, i, visited);
-            }
-        }
-
-        for (int i = 0; i < m; i++) {
-            if (board[i][0] == 'O') {
-                //q.push({i, 0});
-                dfs(board, m, n, i, 0, visited);
-            }
-
-            if (board[i][n-1] == 'O') {
-                //q.push({i, n-1});
-                dfs(board, m, n, i, n-1, visited);
-            }
-        }
-
-        // mark the board
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                int idx = i * n + j;
-
-                if (visited[idx] == 0) {
-                    board[i][j] = 'X';
-                }
-            }
+        if (rank[px] < rank[py])
+            parent[px] = py;
+        else if (rank[px] > rank[py])
+            parent[py] = px;
+        else {
+            parent[py] = px;
+            rank[px]++;
         }
     }
 };
 
 
-/*
-An 'O' is surrounded if there is NO path from it to the boundary of the matrix 
-(ie. row index 0, column index 0, row index matrix.length-1, column index matrix[0].length-1)
-Boundary elements se start kr
-Keep track of all the cells accessible from boundary cells
-Don't mark this cells 'X'
-*/
+class Solution {
+public:
+    void solve(vector<vector<char>>& board) {
+        int m = board.size();
+        int n = board[0].size();
+
+        DSU dsu(m * n + 1);
+        int dummy = m * n;
+
+        vector<vector<int>> dirs = {
+            {0,1}, {1,0}, {-1,0}, {0,-1}
+        };
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] != 'O') continue;
+
+                int id = i * n + j;
+
+                // boundary → connect to dummy
+                if (i == 0 || j == 0 || i == m-1 || j == n-1) {
+                    dsu.unite(id, dummy);
+                }
+
+                // union with neighboring O's
+                for (auto& d : dirs) {
+                    int ni = i + d[0];
+                    int nj = j + d[1];
+
+                    if (ni >= 0 && nj >= 0 && ni < m && nj < n &&
+                        board[ni][nj] == 'O') {
+                        int nid = ni * n + nj;
+                        dsu.unite(id, nid);
+                    }
+                }
+            }
+        }
+
+        // flip all O's not connected to dummy
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') {
+                    int id = i * n + j;
+                    if (dsu.find(id) != dsu.find(dummy)) {
+                        board[i][j] = 'X';
+                    }
+                }
+            }
+        }
+    }
+};
