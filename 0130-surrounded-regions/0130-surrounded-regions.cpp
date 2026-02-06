406f95d0f4a741a8e37f57f790554d55f932fa70
gauravmundhada <148524422+gauravmundhada@users.noreply.github.com>
2026-02-06 14:57:12 +0530

    Time: 0 ms (100%), Space: 14.3 MB (68.93%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -4,49 +4,50 @@ public:
         {0,1}, {1, 0}, {-1, 0}, {0, -1}
     };
 
+    void dfs(vector<vector<char>>& board, int m, int n, int i, int j, vector<int>& visited) {
+        visited[i*n+j] = 1;
+
+        for (auto& d : directions) {
+                int nx = i + d[0];
+                int ny = j + d[1];
+
+                // out of bound + not accessible + already processed cell
+                if (nx < 0 || ny < 0 || nx >= m || ny >= n || board[nx][ny] == 'X' || visited[nx*n+ny] == 1) continue;
+
+                dfs(board, m, n, nx, ny, visited);
+            }
+    }
+
     void solve(vector<vector<char>>& board) {
         int m = board.size();
         int n = board[0].size();
 
         // keep track of cell which is not to be marked
-        unordered_set<int> st;
+        vector<int> visited(m*n, 0);
 
-        queue<pair<int, int>> q;
+        //queue<pair<int, int>> q;
 
         for (int i = 0; i < n; i++) {
             if (board[0][i] == 'O') {
-                q.push({0, i});
+                //q.push({0, i});
+                dfs(board, m, n, 0, i, visited);
             }
 
             if (board[m-1][i] == 'O') {
-                q.push({m-1, i});
+                //q.push({m-1, i});
+                dfs(board, m, n, m-1, i, visited);
             }
         }
 
         for (int i = 0; i < m; i++) {
             if (board[i][0] == 'O') {
-                q.push({i, 0});
+                //q.push({i, 0});
+                dfs(board, m, n, i, 0, visited);
             }
 
             if (board[i][n-1] == 'O') {
-                q.push({i, n-1});
-            }
-        }
-
-        while (!q.empty()) {
-            auto[x ,y] = q.front();
-            q.pop();
-
-            st.insert(x*n+y);
-
-            for (auto& d : directions) {
-                int nx = x + d[0];
-                int ny = y + d[1];
-
-                // out of bound + not accessible + already processed cell
-                if (nx < 0 || ny < 0 || nx >= m || ny >= n || board[nx][ny] == 'X' || st.count(nx*n+ny)) continue;
-
-                q.push({nx, ny});
+                //q.push({i, n-1});
+                dfs(board, m, n, i, n-1, visited);
             }
         }
 
@@ -55,7 +56,7 @@ public:
             for (int j = 0; j < n; j++) {
                 int idx = i * n + j;
 
-                if (!st.count(idx)) {
+                if (visited[idx] == 0) {
                     board[i][j] = 'X';
                 }
             }
