8910d59b18a004cf8a565f088085574c415b8100
gauravmundhada <148524422+gauravmundhada@users.noreply.github.com>
2026-09-06 13:43:28 +0530

    Time: 13 ms (40.99%), Space: 202.7 MB (66.09%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cpp
@@ -1,15 +1,11 @@
+// O(n) space
 class Solution {
 public:
     int firstStableIndex(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> maxel(n);
+        int maxel = INT_MIN;
         vector<int> minel(n);
         int instability_score = k+1;
-
-        for (int i = 0; i <= n-1; i++) {
-            if (i == 0) maxel[i] = nums[i];
-            else maxel[i] = max(maxel[i-1], nums[i]);
-        }
 
         for (int i = n-1; i >= 0; i--) {
             if (i == n-1) minel[i] = nums[i];
@@ -17,7 +13,8 @@ public:
         }
 
         for (int i = 0; i < n; i++) {
-            instability_score = maxel[i] - minel[i];
+            maxel = max(maxel, nums[i]);
+            instability_score = maxel - minel[i];
 
             if (instability_score <= k)
                 return i;
